d567dfd7c3b88c170a39529fb2df167ad93951b1
Zlata Zhilinko <zzhilinko@yandex.ru>
2025-01-13 02:20:12 +0300

    refactor: сборка проекта с webpack и babel

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
+import addIcon from './images/add-icon.svg';
+import avatar from './images/avatar.jpg';
+import card1 from './images/card_1.jpg';
+import card2 from './images/card_2.jpg';
+import card3 from './images/card_3.jpg';
+import close from './images/close.svg';
+import deleteIcon from './images/delete-icon.svg';
+import editIcon from './images/edit-icon.svg';
+import likeActive from './images/like-active.svg';
+import likeInactive from './images/like-inactive.svg';
+import logo from './images/logo.svg';
+import './pages/index.css';
+
+const initialCards = [
+    {
+      name: "Архыз",
+      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+    },
+    {
+      name: "Челябинская область",
+      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+    },
+    {
+      name: "Иваново",
+      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+    },
+    {
+      name: "Камчатка",
+      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+    },
+    {
+      name: "Холмогорский район",
+      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+    },
+    {
+      name: "Байкал",
+      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+    }
+];
+
 const profileName = document.querySelector('.profile__title');
 const profileJob = document.querySelector('.profile__description');
 const cardName = document.querySelector('.card__title');
